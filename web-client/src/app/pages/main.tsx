e2ab9d198e7b456d6a9b4df30eb054a5b424bc59
quentin <quentin.gerard@epitech.eu>
2025-10-06 13:45:47 +0200

    feat: add SignUp page and routing; update styles for font loading

--- web-client/src/app/pages/main.tsx
+++ web-client/src/app/pages/main.tsx
@@ -2,9 +2,24 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
+import SignUp from './SignUp.tsx';
+import Login from './Login.tsx';
+
+function Router() {
+  const path = window.location.pathname;
+
+  switch (path) {
+    case '/signup':
+      return <SignUp />;
+    case '/login':
+      return <Login />;
+    default:
+      return <App />;
+  }
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Router />
   </StrictMode>,
 );
