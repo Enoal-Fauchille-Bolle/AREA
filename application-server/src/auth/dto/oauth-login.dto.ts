4ae4be677214d219fd76363186949f7137dc0b18
Renaud Manet <renaud.manet@epitech.eu>
2025-10-25 16:16:53 +0200

    fix(application-server): add class validators to auth sign-in/log-in DTOs

--- application-server/src/auth/dto/oauth-login.dto.ts
+++ application-server/src/auth/dto/oauth-login.dto.ts
@@ -1,6 +1,32 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsEnum, IsNotEmpty, IsUrl } from 'class-validator';
+import { OAuthProviderServiceNameMap } from '../../oauth2';
+
 export class OAuthLoginDto {
+  @ApiProperty({
+    description:
+      'The Authorization code received from the OAuth provider for login',
+    example: 'SplxlOBeZQQYbYS6WxSbIA',
+  })
+  @IsString()
+  @IsNotEmpty()
   code: string;
+
+  @ApiProperty({
+    description: 'The OAuth provider to use for login',
+    enum: Object.values(OAuthProviderServiceNameMap),
+    example: 'Google',
+  })
+  @IsEnum(Object.values(OAuthProviderServiceNameMap))
+  @IsNotEmpty()
   provider: string;
+
+  @ApiProperty({
+    description: 'The redirect URI used in the OAuth flow',
+    example: 'https://yourapp.com/oauth/callback',
+  })
+  @IsUrl()
+  @IsNotEmpty()
   redirect_uri: string;
 }
 
